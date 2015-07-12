e0ce3294d30e2c4d935c18d2c7cc58b37ed62e26
Shidfar <shidfarhf@yahoo.com>
2015-07-12 11:52:37 +0800

    modified:   .idea/workspace.xml 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,18 @@
 #include <string.h>
 #include <mysql/mysql.h>
 #include <time.h>
+#include <chrono>
+#include <thread>
 using namespace std;
 string exec(char* cmd)
 {
 //    Replace popen and pclose with _popen and _pclose for Windows.
     FILE* pipe = popen(cmd, "r");
+
     if (!pipe) return "ERROR";
     char buffer[128];
     string result = "";
+
     while(!feof(pipe)) {
         if(fgets(buffer, 128, pipe) != NULL)
             result += buffer;
@@ -22,9 +26,12 @@ string exec(char* cmd)
 
 int main()
 {
-    string res = exec("ifconfig");
-    cout<<" *********************** " <<endl;
-    cout<<" >> " <<res<<endl;
-    cout<<" *********************** " <<endl;
+    string res = exec("ifconfig | perl -nle'/dr:(\\S+)/ && print $1'");
+    cout<<" > Internal address:" <<endl;
+    cout<<res<<endl;
+    cout<<" > External address:" <<endl;
+    res = exec("dig +short myip.opendns.com @resolver1.opendns.com");
+    cout<<res<<endl;
+
     return 0;
 }
